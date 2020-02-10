4efb919794c8e2786b687000873ca872b0d72140
Andres Carvajal <jc1993box@hotmail.com>
2020-02-10 18:23:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,16 @@
 class Book
   attr_accessor :author, :page_count, :genre
 
-def initialize(title)
-  @title=title
- 
-end
+ def initialize(title)
+   @title=title
+ end
 
-def title
-  @title
-end
+ def title
+   @title
+ end
 
-def turn_page
-  puts "f"
+ def turn_page
+   puts "fFlipping the page...wow, you read fast!"
+ end
+
+end
