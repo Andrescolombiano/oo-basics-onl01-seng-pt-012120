f0d35b773585f7115798cd8aaf017236a742675c
Andres Carvajal <jc1993box@hotmail.com>
2020-02-10 18:22:31 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,25 +1,14 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+
 def initialize(title)
   @title=title
-  @author=author
-  @page=page
-  @genre=genre
+ 
 end
 
 def title
   @title
 end
 
-def author 
-  @author 
-end
-
-def page 
-  @page
-end
-
-def genre 
-  @genre 
-end 
-
-end
+def turn_page
+  puts "f"
